4cfc8e592a34130599d4f0659bd817ea36fa2267
Sasikalaa Soundarajan <156901771+Sasikalaas28@users.noreply.github.com>
2024-07-23 14:45:37 +0530

    Update stats.h
    Added structure of Stats

--- stats.h
+++ stats.h
@@ -3,5 +3,8 @@
 namespace Statistics {
     // define the Stats structure here. See the tests to infer its properties
     
-    Stats ComputeStatistics(const std::vector<___>& );
+    Stats ComputeStatistics(const std::vector<double>& );
+    struct Stats{
+    double 
+}
 }
