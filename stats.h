ce997d8179f1a6a15d3e00f06a17a492ce9f82b4
Sasikalaa Soundarajan <156901771+Sasikalaas28@users.noreply.github.com>
2024-07-23 15:03:59 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -3,7 +3,7 @@
 namespace Statistics {
     // define the Stats structure here. See the tests to infer its properties
     
-    Stats ComputeStatistics(const std::vector<double>& );
+    Stats ComputeStatistics(const std::vector<double>&);
     struct Stats{
     double average;
     double max;
