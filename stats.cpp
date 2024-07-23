270ffdaf03b119007b03dd8bbe6e565878abe695
Sasikalaa Soundarajan <156901771+Sasikalaas28@users.noreply.github.com>
2024-07-23 15:02:57 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -2,15 +2,20 @@
 
 Stats Statistics::ComputeStatistics(const std::vector<double>& in) {
     //Implement statistics here
-    int len = in.size();
-    int sum = 0.0;
+    int len = in.size(); //vector size
+    double sum = 0.0; //sum of the vector elements
     Stats st;
     st.min = in[0];
     st.max = in[0];
-    st.
+  //  st.average = 0.0;
     for(int i=0;i<len;i++)
         {
-            
-            
+            if(in[i] < st.min)
+                st.min = in[i];
+            if(in[i] > st.max)
+                st.max = in[i];
+        sum += in[i];   
         }
+    st.average = sum/len;
+    return st;
 }
